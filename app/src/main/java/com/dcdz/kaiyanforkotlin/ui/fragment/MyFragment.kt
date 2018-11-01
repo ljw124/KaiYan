00968b659999e8cc79346b8292e4da9de9c727d3
ljw124 <929377025@qq.com>
2018-11-01 13:15:23 +0800

    Improve "my" module

--- app/src/main/java/com/dcdz/kaiyanforkotlin/ui/fragment/MyFragment.kt
+++ app/src/main/java/com/dcdz/kaiyanforkotlin/ui/fragment/MyFragment.kt
@@ -1,8 +1,11 @@
 package com.dcdz.kaiyanforkotlin.ui.fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import com.dcdz.kaiyanforkotlin.R
 import com.dcdz.kaiyanforkotlin.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_my.*
 
 /**
  * Created by LJW on 2018/11/1.
@@ -24,6 +27,21 @@ class MyFragment : BaseFragment() {
     override fun getLayoutId(): Int = R.layout.fragment_my
 
     override fun initView() {
+        tvBlobUrl.setOnClickListener{
+            val uri = Uri.parse("https://blog.csdn.net/ljw124213")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            activity!!.startActivity(intent)
+        }
+        tvGithubUrl.setOnClickListener {
+            val uri = Uri.parse("https://github.com/ljw124/KaiYan")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            activity!!.startActivity(intent)
+        }
+        tvEmailUrl.setOnClickListener {
+            val uri = Uri.parse("https://github.com/ljw124/KaiYan")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            activity!!.startActivity(intent)
+        }
     }
 
     override fun lazyLoad() {
